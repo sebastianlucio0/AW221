21e8b8f903f6e3ba7b7e8565a5ca7278d12561e3
sebastianlucio0 <125054977@upq.edu.mx>
2026-09-17 15:32:33 -0600

    PHP y HTML
    Vimos php y html

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo "Hola Mundo!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            color:blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola Crayola</h1>
+    <h2>Hola Crayola</h2>
+    <h3>Hola Crayola</h3>
+    <h4>Hola Crayola</h4>
+    <h5>Hola Crayola</h5>
+    <h6>Hola Crayola</h6>
+
+
+    
+
+</body>
+</html>
