c5bd30eeee946a9af04bb596f0e85a3c93a9ff85
sebastianlucio0 <125054977@upq.edu.mx>
2026-09-18 17:28:54 -0600

    PRACTICA HTML

--- index.php
+++ index.php
@@ -9,21 +9,37 @@ echo "Hola Mundo!";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        h1 {
-            color:blue;
+        .seccion {
+            background-color: #369055;
+            padding: 20px 40px;
+            margin: 20px;
+            border: 10px hidden black;
+        }
+
+
+        .seccion2 {
+            background-color: #bd5757;
+            padding-top: 40px;
+            padding-right: 20px;
+            padding-bottom: 40px;
+            padding-left: 20px;
+            margin-top: 20px;
+            margin-right: 30px;
+            margin-left: 50px;
+            margin-bottom: 40px;
         }
     </style>
 </head>
 <body>
-    <h1>Hola Crayola</h1>
-    <h2>Hola Crayola</h2>
-    <h3>Hola Crayola</h3>
-    <h4>Hola Crayola</h4>
-    <h5>Hola Crayola</h5>
-    <h6>Hola Crayola</h6>
+    <div class="seccion">
+        <h1>Titulo del parrafo</h1>
+        <p>lorem ipsum dolor sit amet</p>
 
+    </div>
 
-    
-
+    <div class="seccion2">
+        <h2>Titulo del parrafo</h2>
+        <p>lorem ipsum dolor sit amet</p>
+    </div>
 </body>
 </html>
